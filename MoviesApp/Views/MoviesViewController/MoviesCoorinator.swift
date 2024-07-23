de2ac7896721c1a17b821bfd2b2f541d89f26ef4
Olga Lutsyk <olyalutsyk@gmail.com>
2024-07-23 20:38:36 +0300

    Remove force unwraps

--- MoviesApp/Views/MoviesViewController/MoviesCoorinator.swift
+++ MoviesApp/Views/MoviesViewController/MoviesCoorinator.swift
@@ -18,41 +18,44 @@ let container = Container { container in
         NetworkClient() as NetworkProtocol
     }
     container.register(MovieService.self) { resolver in
-        let networkClient = resolver.resolve(NetworkProtocol.self)!
+        guard let networkClient = resolver.resolve(NetworkProtocol.self) else {
+            fatalError("Failed to init NetworkProtocol")
+        }
         return MovieService(networkClient: networkClient)
     }
 }
 
 final class MoviesCoorinator {
     var navigationController: UINavigationController
-
+    
     // MARK: Internal methods
-
+    
     init(with navigation: UINavigationController) {
         navigationController = navigation
     }
-
+    
     func start() {
         let landingViewController = createLandingViewController()
         navigationController.pushViewController(landingViewController, animated: true)
     }
-
+    
     // MARK: Private methods
-
+    
     private func createLandingViewController() -> UIViewController {
-       
-        let movieService = container.resolve(MovieService.self)!
-               let context = MovieViewModelImpl.ServiceContext(service: movieService,
-                                                               builder: MoviesBuilderImpl())
+        guard let movieService = container.resolve(MovieService.self) else {
+            fatalError("Failed to create MovieService")
+        }
+        let context = MovieViewModelImpl.ServiceContext(
+            service: movieService,
+            builder: MoviesBuilderImpl())
         let handlers = MovieViewModelImpl.Handlers { [weak self] id in
-            guard let self = self else {return}
+            guard let self = self else { return }
             self.showDetails(id: id)
         }
+        let viewModel = MovieViewModelImpl(
+            context: context,
+            handlers: handlers)
         
-
-        let viewModel = MovieViewModelImpl(context: context,
-                                           handlers: handlers)
-
         let vc = Inject.ViewControllerHost(MoviesViewController(with: viewModel))
         return vc
     }
